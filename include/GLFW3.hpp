ad5f8606b0645651ca0159c55d56436e20cdebe6
Christian Janke <herr.christian.janke@web.de>
2019-08-17 20:50:20 +0200

    glfw library init

--- include/GLFW3.hpp
+++ include/GLFW3.hpp
@@ -1,5 +1,7 @@
 #include <GLFW/glfw3.h>
 
+#include <stdexcept>
+
 namespace glfw {
 
 
@@ -26,4 +28,40 @@ namespace glfw {
 	inline void post_empty_event() {
 		glfwPostEmptyEvent();
 	}
+	
+		/************************************************************************************
+	 *																					*
+	 *									 LIBRARY INIT									*
+	 *																					*
+	 ************************************************************************************/
+
+	enum class init_hint_type : int {
+		JOYSTICK_HAT_BUTTONS = GLFW_JOYSTICK_HAT_BUTTONS,
+		COCOA_CHDIR_RESOURCES = GLFW_COCOA_CHDIR_RESOURCES,
+		COCOA_MENUBAR = GLFW_COCOA_MENUBAR,
+	};
+
+	struct init_hint {
+		init_hint_type hint;
+		bool hintValue;
+	};
+
+	class glfw_lib {
+		glfw_lib() {
+			if (!glfwInit()) throw std::runtime_error("Failed to init GLFW");
+		}
+		~glfw_lib() {
+			glfwTerminate();
+		}
+
+	public:
+		template<class ...hints>
+		static void init_hints(hints... initHints) {
+			static_assert((std::is_same_v<init_hint, hints> && ...));
+			(glfwInitHint(static_cast<int>(initHints.hint), initHints.hintValue ? TRUE : FALSE), ...);
+		}
+		static void init() {
+			static glfw_lib libInstance;
+		}
+	};
 }
